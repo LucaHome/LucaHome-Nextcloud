52066d29b698aac64ffac6c0a24f571732b0aa05
Jonas Schubert <guepardoapps@gmail.com>
2019-02-03 20:44:21 +0100

    Create ErrorCode.php

---
+++ lucahome/lib/Enums/ErrorCode.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Helpful: https://stackoverflow.com/questions/254514/php-and-enumerations
+ */
+
+namespace OCA\LucaHome\Enums;
+
+abstract class ErrorCode {
+    const WirelessSocketCodeAlreadyInUse = -100;
+}
