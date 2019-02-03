e0753dd03c70c9d6af8f3b983501816be7e01978
Jonas Schubert <guepardoapps@gmail.com>
2019-02-03 18:27:29 +0100

    Create WirelessSocket.php

---
+++ lucahome/lib/Entities/WirelessSocket.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Helpful: https://docs.nextcloud.com/server/stable/developer_manual/app/storage/database.html
+ */
+
+namespace OCA\LucaHome\Db;
+
+use OCP\AppFramework\Db\Entity;
+
+class WirelessSocket extends Entity {
+
+    protected $name;
+    protected $code;
+    protected $area;
+    protected $state;
+
+    protected $userId;
+    protected $description;
+    protected $public;
+    protected $added;
+    protected $lastmodified;
+    protected $clickcount;
+
+    public function __construct() {
+        $this->addType('state', 'integer');
+        $this->addType('public', 'integer');
+        $this->addType('added', 'integer');
+        $this->addType('lastmodified', 'integer');
+        $this->addType('clickcount', 'integer');
+    }
+}
