3de17c941245c2f791569160872782732f0ebecd
Jonas Schubert <guepardoapps@gmail.com>
2019-02-03 20:51:44 +0100

    Update ErrorCode.php

--- lucahome/lib/Enums/ErrorCode.php
+++ lucahome/lib/Enums/ErrorCode.php
@@ -8,5 +8,10 @@ namespace OCA\LucaHome\Enums;
 
 abstract class ErrorCode {
     const NoError = 0;
+
     const WirelessSocketCodeAlreadyInUse = -100;
+    const WirelessSocketNameTooLong = -101;
+    const WirelessSocketCodeLengthInvalid = -102;
+    const WirelessSocketAreaTooLong = -103;
+    const WirelessSocketDescriptionTooLong = -104;
 }
