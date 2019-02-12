9574a0a238d1dabae0f73de0c8251f0558fc8046
Jonas Schubert <guepardoapps@gmail.com>
2019-02-12 08:33:40 +0100

    experiment with vue material

--- lucahome/src/main.js
+++ lucahome/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {
+  MdDrawer
+} from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
 Vue.config.productionTip = false
+
+Vue.use(MdDrawer)
 
 new Vue({
   render: h => h(App),
