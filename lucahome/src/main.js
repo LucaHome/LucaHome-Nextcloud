cddcee2b5fcba5e9611125d884332e9d9bde8e97
Jonas Schubert <guepardoapps@gmail.com>
2019-02-13 18:13:29 +0100

    Update main.js

--- lucahome/src/main.js
+++ lucahome/src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store/store'
 
-import {
-  MdDrawer
-} from 'vue-material/dist/components'
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
 
-Vue.config.productionTip = false
-
-Vue.use(MdDrawer)
+Vue.use(VueMaterial)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  store,
+  render: h => h(App)
+})
