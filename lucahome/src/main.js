41e543e2a59e3f50397f2b10e9a376fe8d3e739c
Jonas Schubert <guepardoapps@gmail.com>
2019-02-13 22:07:01 +0100

    use vuelidate

--- lucahome/src/main.js
+++ lucahome/src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import store from './store/store'
 
 import VueMaterial from 'vue-material'
+import Vuelidate from 'vuelidate'
+
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default-dark.css'
 
 Vue.use(VueMaterial)
+Vue.use(Vuelidate)
 
 new Vue({
   el: '#app',
