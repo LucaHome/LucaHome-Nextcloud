a118be03751fa167b8c1b16a2227f36285285405
Jonas Schubert <guepardoapps@gmail.com>
2019-02-13 21:41:21 +0100

    Update store.js

--- lucahome/src/store/store.js
+++ lucahome/src/store/store.js
@@ -5,52 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        view: "list",
-        wirelessSockets: [],
-        newWirelessSocket: {}
+        wirelessSocketList: [],
+        selectedWirelessSocket: {}
     },
     mutations: {
-        CHANGE_VIEW(state, view) {
-            state.view = view
+        SELECT_WIRELESS_SOCKET(state, wirelessSocket) {
+            state.selectedWirelessSocket = wirelessSocket
         },
-        GET_WIRELESS_SOCKET(state, wirelessSocket) {
-            state.newWirelessSocket = wirelessSocket
-        },
-        ADD_WIRELESS_SOCKET(state) {
-            state.wirelessSockets.push(state.newWirelessSocket)
-        },
-        EDIT_WIRELESS_SOCKET(state, wirelessSocket) {
-            var wirelessSockets = state.wirelessSockets
-            wirelessSockets.splice(wirelessSockets.indexOf(wirelessSocket), 1)
-            state.wirelessSockets = wirelessSockets
-            state.newWirelessSocket = wirelessSocket
+        ADD_WIRELESS_SOCKET(state, wirelessSocket) {
+            state.wirelessSocketList.push(wirelessSocket)
         },
         REMOVE_WIRELESS_SOCKET(state, wirelessSocket) {
-            var wirelessSockets = state.wirelessSockets
-            wirelessSockets.splice(wirelessSockets.indexOf(wirelessSocket), 1)
-            state.wirelessSockets = wirelessSockets
+            var wirelessSocketList = state.wirelessSocketList
+            wirelessSocketList.splice(wirelessSocketList.indexOf(wirelessSocket), 1)
+            state.wirelessSocketList = wirelessSocketList
         }
     },
     actions: {
-        changeView({commit}, view) {
-            commit('CHANGE_VIEW', view)
-        },
-        getWirelessSocket({commit}, wirelessSocket) {
-            commit('GET_WIRELESS_SOCKET', wirelessSocket)
+        selectWirelessSocket({commit}, wirelessSocket) {
+            commit('SELECT_WIRELESS_SOCKET', wirelessSocket)
         },
         addWirelessSocket({commit}) {
             commit('ADD_WIRELESS_SOCKET')
-        },
-        editWirelessSocket({commit}, wirelessSocket) {
-            commit('EDIT_WIRELESS_SOCKET', wirelessSocket)
         },
         removeWirelessSocket({commit}, wirelessSocket) {
             commit('REMOVE_WIRELESS_SOCKET', wirelessSocket)
         }
     },
     getters: {
-        view: state => state.view,
-        wirelessSockets: state => state.wirelessSockets,
-        newWirelessSocket: state => state.newWirelessSocket
+        wirelessSocketList: state => state.wirelessSocketList,
+        selectedWirelessSocket: state => state.selectedWirelessSocket
     }
 })
