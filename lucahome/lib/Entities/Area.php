ef7b500a182b62c2adb6543bf179707c46853c5d
Jonas Schubert <guepardoapps@gmail.com>
2019-02-15 23:52:59 +0100

    Create Area.php

---
+++ lucahome/lib/Entities/Area.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Helpful: https://docs.nextcloud.com/server/stable/developer_manual/app/storage/database.html
+ */
+
+namespace OCA\LucaHome\Entities;
+
+use OCP\AppFramework\Db\Entity;
+
+class Area extends Entity {
+    protected $name;
+    protected $filter;
+}
