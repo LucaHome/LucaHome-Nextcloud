645cee47d52a6eb85037ffff960d2d98812f4377
Jonas Schubert <guepardoapps@gmail.com>
2019-02-15 22:21:13 +0100

    Create AreaListView.vue

---
+++ lucahome/src/components/AreaListView.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <md-list class="md-single-line">
+      <div v-for="(area, index) in areaList" :key="index">
+        <md-list-item
+          :class="{selected: area.id === selectedArea.id, selectable: area.id !== selectedArea.id}"
+        >
+          <div v-if="area.name" class="md-list-item-text" @click="select(area)">
+            <span>{{area.name}}</span>
+          </div>
+
+          <md-field v-else>
+            <label>Name</label>
+            <md-input v-model="newAreaName"></md-input>
+            <md-button class="md-icon-button save-button md-primary" @click="saveArea">
+              <md-icon>save</md-icon>
+            </md-button>
+          </md-field>
+        </md-list-item>
+
+        <md-divider class="md-inset"></md-divider>
+      </div>
+    </md-list>
+
+    <md-button class="md-icon-button md-raised add-button md-primary" @click="addArea">
+      <md-icon>add</md-icon>
+    </md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AreaListView",
+  data: () => ({
+    newAreaName: null
+  }),
+  methods: {
+    select(area) {
+      this.$store.dispatch("selectArea", area);
+    },
+    addArea() {
+      this.$store.dispatch("addArea");
+    },
+    saveArea() {
+      if (this.newAreaName.length > 0) {
+        this.$store.dispatch("saveArea", this.newAreaName);
+      }
+    }
+  },
+  computed: {
+    areaList() {
+      return this.$store.getters.areaList;
+    },
+    selectedArea() {
+      return this.$store.getters.selectedArea;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.md-list {
+  min-width: 15rem;
+  width: 15rem;
+  max-width: 15rem;
+  display: inline-block;
+  vertical-align: top;
+}
+
+.add-button {
+  position: fixed;
+  left: 11rem;
+  bottom: 1rem;
+}
+
+.save-button {
+  cursor: pointer;
+  right: 1.5rem;
+}
+</style>
