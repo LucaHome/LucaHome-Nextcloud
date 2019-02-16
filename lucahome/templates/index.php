9533a684888f0173c4f6a2eb29df67f753929ad8
Jonas Schubert <guepardoapps@gmail.com>
2019-02-16 17:23:55 +0100

    clean up templates

---
+++ lucahome/templates/index.php
@@ -0,0 +1,17 @@
+<?php
+script('lucahome', 'app.3c177c5b');
+style('lucahome', 'app.7e352abe');
+?>
+
+<div id="app">
+	<div id="app-navigation">
+		<?php print_unescaped($this->inc('navigation/index')); ?>
+		<?php print_unescaped($this->inc('settings/index')); ?>
+	</div>
+
+	<div id="app-content">
+		<div id="app-content-wrapper">
+			<?php print_unescaped($this->inc('content/index')); ?>
+		</div>
+	</div>
+</div>
