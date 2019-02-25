47d227006adf00fca8ee8626ec98559613f666a5
Jonas Schubert <guepardoapps@gmail.com>
2019-02-25 18:13:21 +0100

    adds local material design icons and font awesome

--- lucahome/.stylelintrc.js
+++ lucahome/.stylelintrc.js
@@ -1,25 +1,11 @@
 module.exports = {
 	rules: {
-		indentation: 'tab',
 		'selector-type-no-unknown': null,
 		'number-leading-zero': null,
-		'rule-empty-line-before': [
-			'always',
-			{
-				ignore: ['after-comment', 'inside-block']
-			}
-		],
-		'declaration-empty-line-before': [
-			'never',
-			{
-				ignore: ['after-declaration']
-			}
-		],
 		'comment-empty-line-before': null,
 		'selector-type-case': null,
 		'selector-list-comma-newline-after': null,
-		'no-descending-specificity': null,
-		'string-quotes': 'single'
+		'no-descending-specificity': null
 	},
 	plugins: ['stylelint-scss']
 }
