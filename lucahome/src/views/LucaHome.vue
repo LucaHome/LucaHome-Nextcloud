8773d8ef3a66eac33fbdbc079e6d866d9f0472d3
Jonas Schubert <guepardoapps@gmail.com>
2019-02-25 11:29:40 +0100

    fixes layout

--- lucahome/src/views/LucaHome.vue
+++ lucahome/src/views/LucaHome.vue
@@ -1,22 +1,11 @@
 <template>
   <app-content app-name="lucahome">
-    <link
-      rel="stylesheet"
-      href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons"
-    >
-    <link
-      rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-      crossorigin="anonymous"
-    >
-
     <template slot="navigation">
       <AreaListView class="area-list-view"/>
-      <WirelessSocketListView class="wireless-socket-list-view"/>
     </template>
 
     <template slot="content">
+      <WirelessSocketListView class="wireless-socket-list-view"/>
       <WirelessSocketDetailView class="detail-view"/>
     </template>
   </app-content>
